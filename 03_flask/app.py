0b612dbd6b7fa729f3ebe289f1f3d95becd546c7
hima-del <himajaharidas77@gmail.com>
2021-01-29 19:03:31 +0530

    flask

--- 03_flask/app.py
+++ 03_flask/app.py
@@ -1,5 +1,18 @@
-from flask import Flask
+from flask import Flask,redirect,url_for
+
 app=Flask(__name__)
+
 @app.route("/")
 def home():
-    return "hello!"
+    return "hello! this is the main page <h1>hello</h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello{name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home")) 
+
+if __name__=="__main__":
+    app.run()       
